193c4273089e9736d0b2062fe440127594f51104
Kirti Sinha <kirti.sinha16@gmail.com>
2025-06-14 07:50:16 -0400

    Add files via upload

---
+++ March 17 - Basic Python/test.py
@@ -0,0 +1,14 @@
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# TO ADD NUMBERS RANDOMLY AT COMMAND PROMPT
+
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+d = int(sys.argv[4])
+z = a + b +c + d
+print(z)
